5c7a4e2b117079aad9b678a2ffad0140897b3247
Abdul <aboodmm@gmail.com>
2016-01-25 10:57:04 -0800

    more fixes

--- stack/stack.go
+++ stack/stack.go
@@ -8,18 +8,18 @@ type Stack struct {
 	top        int
 }
 
-func newStack(m int) Stack {
+func newStack(m int) *Stack {
 	s := new(Stack)
 	s.maxSize = m
 	s.top = -1
 	return s
 }
 
-func Push(n string) {
-	top += 1
-	stackArray[top] = n
+func Push(n string, s *Stack) {
+	s.top += 1
+	s.stackArray[s.top] = n
 }
 
-func Pop() struct{} {
-	return stackArray
+func Pop(s *Stack) []struct{} {
+	return s.stackArray
 }
