ebe49dcad2764b6e4310eb485e4f300936bac00c
Abdul <aboodmm@gmail.com>
2016-01-25 17:50:06 -0800

    YESS YESS

--- bin/main.go
+++ bin/main.go
@@ -5,5 +5,5 @@ import "github.com/aboodmm/todo"
 
 func main() {
 	s := todo.NewStack(5)
-	fmt.Printf("HELLO %d", s.maxSize)
+	fmt.Printf("HELLO %d", s.MaxSize)
 }
--- stack.go
+++ stack.go
@@ -3,24 +3,24 @@ package todo
 import "time"
 
 type Stack struct {
-	maxSize     int
-	stackArray  []Item
-	top         int
-	dateCreated time.Time
+	MaxSize     int
+	StackArray  []Item
+	Top         int
+	DateCreated time.Time
 }
 
 func NewStack(m int) *Stack {
 	s := new(Stack)
-	s.maxSize = m
-	s.top = -1
+	s.MaxSize = m
+	s.Top = -1
 	return s
 }
 
 func Push(i Item, s *Stack) {
-	s.top += 1
-	s.stackArray[s.top] = i
+	s.Top += 1
+	s.StackArray[s.Top] = i
 }
 
 func Pop(s *Stack) []Item {
-	return s.stackArray
+	return s.StackArray
 }
